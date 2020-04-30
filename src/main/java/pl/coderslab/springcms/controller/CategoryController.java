3337b3dde7d457aecbe4c019f8bc3c4aaa4bd16d
Marek Mazur <marrekmazur@gmail.com>
2020-04-30 21:21:11 +0200

    created add category actions and view

--- src/main/java/pl/coderslab/springcms/controller/CategoryController.java
+++ src/main/java/pl/coderslab/springcms/controller/CategoryController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.coderslab.springcms.domain.dao.CategoryDao;
 import pl.coderslab.springcms.domain.model.Category;
@@ -26,6 +27,19 @@ public class CategoryController {
         model.addAttribute("categories", categories);
         return "categories/list";
 
+    }
+
+    @GetMapping("/add")
+    public String addCategoryForm(Model model){
+        Category category=new Category();
+        model.addAttribute("category", category);
+        return "categories/add";
+    }
+
+    @PostMapping("/add")
+    public String processAddCategoryForm(Category category){
+        categoryDao.create(category);
+        return "redirect:/categories";
     }
 
 
---
+++ src/main/webapp/WEB-INF/views/categories/add.jsp
@@ -0,0 +1,32 @@
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+<%--
+  Created by IntelliJ IDEA.
+  User: marek
+  Date: 30.04.2020
+  Time: 21:13
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Add category</title>
+</head>
+<body>
+
+<h3><a href="/categories">Show all categories</a> </h3>
+
+<form:form method="post" modelAttribute="category">
+
+    <form:label path="name">Name: </form:label>
+    <form:input path="name"/>
+
+    <form:label path="description">Description: </form:label>
+    <form:input path="description"/>
+
+    <form:button>Add</form:button>
+
+
+</form:form>
+
+</body>
+</html>
