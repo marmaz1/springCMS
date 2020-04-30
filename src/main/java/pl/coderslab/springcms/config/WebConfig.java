ea3be403658f9adaf2158cf6def28df690be0b77
Marek Mazur <marrekmazur@gmail.com>
2020-04-30 20:22:28 +0200

    create viewResolver bean

--- src/main/java/pl/coderslab/springcms/config/WebConfig.java
+++ src/main/java/pl/coderslab/springcms/config/WebConfig.java
@@ -1,7 +1,18 @@
 package pl.coderslab.springcms.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
 }
