dc63406a8e25d0c7e271b2b398bff28c7cf11b32
Marek Mazur <marrekmazur@gmail.com>
2020-05-05 23:11:48 +0200

    changes in validation configurations

--- src/main/java/pl/coderslab/springcms/config/ValidationConfig.java
+++ src/main/java/pl/coderslab/springcms/config/ValidationConfig.java
@@ -1,4 +1,18 @@
 package pl.coderslab.springcms.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+import javax.validation.Validator;
+
+@Configuration
 public class ValidationConfig {
+
+    @Bean
+    public Validator validator(){
+        LocalValidatorFactoryBean localValidatorFactoryBean=new LocalValidatorFactoryBean();
+        return localValidatorFactoryBean;
+    }
+
 }
